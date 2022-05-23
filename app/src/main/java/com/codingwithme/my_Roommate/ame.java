d4a2619c18538e629327bda6d8209c0e5f811c7b
likker pallavi <pallavireddyvontela31@gmail.com>
2022-05-23 18:57:34 +0530

    Vivek works

---
+++ app/src/main/java/com/codingwithme/my_Roommate/ame.java
@@ -0,0 +1,14 @@
+package com.codingwithme.my_Roommate;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ame extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_ame);
+    }
+}
